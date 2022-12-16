2a8ed3c1ac465ca80b335aa82144ee9f51cf986a
DureginKostya <kot-502@mail.ru>
2022-12-16 18:28:05 +0500

    Added folder with program to Task 4

---
+++ Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на входе 
+три числа и выдает максимальное из этих чисел.*/
+Console.Clear();
+int[] massiv = new int[3];
+for (int index = 0; index < 3; index++)
+{
+    int i = index;
+    Console.Write($"Введите число № {i + 1}: ");
+    massiv [index] = int.Parse(Console.ReadLine()!);
+}
+int maximum = massiv[0];
+for (int index = 1; index < 3; index++)
+{
+    if (maximum < massiv[index])
+    {
+        maximum = massiv[index];
+    }
+}
+Console.WriteLine($"Максимальное число равно {maximum}");
