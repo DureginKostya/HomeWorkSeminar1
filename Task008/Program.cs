857f9aeede5061232f3029bd6f522db4982d20a5
DureginKostya <kot-502@mail.ru>
2022-12-16 19:07:34 +0500

    Added folder with program to Task 8

---
+++ Task008/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все четные числа от 1 до N.*/
+Console.Clear();
+Console.Write("Введите число больше нуля: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 1)
+{
+    int index = 2;
+    Console.Write($"Последовательность четных чисел от 1 до {number}:");
+    while (index <= number)
+    {
+        if (index % 2 == 0)
+        {
+            Console.Write($"  {index}");
+        }
+        index++;
+    }
+    Console.WriteLine();
+}
+else if (number == 1)
+    { 
+        Console.WriteLine("В последовательности четных чисел нет");
+    }
+    else
+    {
+        Console.WriteLine("Введено неправильное число");
+    }
