befc24d34c2a5723694441a4d192edff7ceaac7a
DureginKostya <kot-502@mail.ru>
2022-12-16 18:42:55 +0500

    Added folder with program to Task 6

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число и выдает, 
+является ли число четным (делится ли оно на два без остатка)*/
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
