01ea8419629a46ff478609422ffb077e309e7ada
DureginKostya <kot-502@mail.ru>
2022-12-17 17:28:46 +0500

    Changed code program, type variables double

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,14 +1,14 @@
 ﻿/*Напишите программу, которая принимает на входе 
-три числа и выдает максимальное из этих чисел.*/
+три числа и выдаёт максимальное из этих чисел.*/
 Console.Clear();
-int[] massiv = new int[3];
+double[] massiv = new double[3];
 for (int index = 0; index < 3; index++)
 {
     int i = index;
     Console.Write($"Введите число № {i + 1}: ");
-    massiv [index] = int.Parse(Console.ReadLine()!);
+    massiv [index] = double.Parse(Console.ReadLine()!);
 }
-int maximum = massiv[0];
+double maximum = massiv[0];
 for (int index = 1; index < 3; index++)
 {
     if (maximum < massiv[index])
